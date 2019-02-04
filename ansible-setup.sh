0a3b37f106b1f191b3eddd4e357f0a4b2845c35d
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2019-02-04 17:12:10 -0500

    Update ansible-setup.sh

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -8,10 +8,10 @@ ansible —version
 sudo apt-get install build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y 
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -y
 sudo apt-get install python3.6 -y
-sudo apt-get install python3-pip -y 
+sudo apt-get install python3-pip -y
 pip3 install --upgrade pip
 pip3 install jinja2
 sudo apt-get install python-netaddr -y
@@ -19,3 +19,7 @@ sudo sysctl net.ipv4.ip_forward
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo ufw status
 sudo ufw disable
+git clone https://github.com/kubernetes-incubator/kubespray.git
+cp kube.env kubespray/.
+cp kubespray-install-k8s.sh kubespray/.
+chmod +x kubespray/kubespray-install-k8s.sh
