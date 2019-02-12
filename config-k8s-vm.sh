e758e10825651a1d268c0792598396fc5d03094a
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2019-02-12 10:48:19 -0500

    Update config-k8s-vm.sh

--- config-k8s-vm.sh
+++ config-k8s-vm.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-sudo apt-get update
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -y
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+pip install -upgrade pip
+pip install jinja2
+sudo apt-get install python3.6 -y
+sudo apt-get install python3-pip -y
+pip3 install --upgrade pip
+pip3 install jinja2
+sudo apt-get install python-netaddr -y
 sudo sysctl net.ipv4.ip_forward
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo ufw status
