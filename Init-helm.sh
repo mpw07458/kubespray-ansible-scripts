7a4c461af150f1cb9bab9d80907af5a834639674
mpw07458 <mpw10128@gmail.com>
2019-02-16 22:40:54 -0500

    changes to helm install part 1

--- Init-helm.sh
+++
@@ -1,1 +0,0 @@
-helm init --stable-repo-url https://kubernetes-charts.lcp-devops-us-lenovo.com --service-account tiller  --tiller-image https://storage.lcp-devops-us-lenovo.com/tiller
--- install-rbac-helm-tiller.sh
+++ install-rbac-helm-tiller.sh
@@ -1,4 +1,4 @@
 source vault.env.us
 kubectl create -f k8s-cls-admin.yaml
 kubectl create -f helm-tiller-rbac.yaml
-./Init-helm.sh
+./init-helm.sh
